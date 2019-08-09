b71150cd00d70c456ffefe50a73fa629fd67d936
AzuxDario <pawel_mibor95@tlen.pl>
2019-08-09 21:28:24 +0200

    Added main menu

---
+++ KenneyJam2019/Assets/_Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject OptionsPanel;
+    public GameObject CreditsPanel;
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void OpenOptions()
+    {
+        MainMenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        MainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    public void BackFromOptions()
+    {
+        MainMenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void BackFromCredits()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+}
+
