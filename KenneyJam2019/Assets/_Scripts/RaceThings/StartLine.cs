746ca7e0e44fe414318ad7ff8deee2a0178a0013
Paweł Dziedzic <blavikenn@gmail.com>
2019-08-09 21:17:30 +0200

    Changed location of assets, added some race managing scripts

---
+++ KenneyJam2019/Assets/_Scripts/RaceThings/StartLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLine : MonoBehaviour {
+    public float startLineSize;
+
+    public Vector3[] GetStartPositionsForPlayers(int playersCount) {
+        var positions = new Vector3[playersCount];
+
+        var startPoint = transform.position + transform.right * startLineSize;
+        var endPoint = transform.position - transform.right * startLineSize;
+
+        for (int i = 0; i < playersCount; i++) {
+            positions[i] = Vector3.Lerp(startPoint, endPoint, (float) i / (playersCount - 1));
+        }
+
+        return positions;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.magenta;
+
+        var startPoint = transform.position + transform.right * startLineSize;
+        var endPoint = transform.position - transform.right * startLineSize;
+
+        Gizmos.DrawLine(startPoint, endPoint);
+    }
+}
