67818fac866fe4e684b008b1565eb87415de3cdd
Paweł Dziedzic <blavikenn@gmail.com>
2019-08-09 22:24:14 +0200

    Racing kit, race control point

---
+++ KenneyJam2019/Assets/_Scripts/RaceThings/RaceControlPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceControlPoint : MonoBehaviour {
+    public bool isEndPoint;
+    public RaceControlPoint nextPoint;
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+
+        if (nextPoint != null)
+            Gizmos.DrawLine(transform.position, nextPoint.transform.position);
+    }
+}
