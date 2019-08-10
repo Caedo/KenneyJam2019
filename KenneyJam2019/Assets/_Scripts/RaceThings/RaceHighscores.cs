55965c1aeaf27a950b97d391f5024dc3160e2ab2
AzuxDario <pawel_mibor95@tlen.pl>
2019-08-10 20:22:16 +0200

    Add highscores manager and script

---
+++ KenneyJam2019/Assets/_Scripts/RaceThings/RaceHighscores.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceHighscores : MonoBehaviour
+{
+    public RaceManager raceManager;
+
+    public List<float> highscores { get; private set; }
+
+    void Awake()
+    {
+        ShipRaceController.OnFinishedRace += OnFinishedRace;
+        highscores = new List<float>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnFinishedRace(ShipRaceController shipRaceController)
+    {
+        if(shipRaceController.playerData.steerByAI == false)
+        {
+            // We have player
+            highscores.Add(raceManager.timeSinceRaceStarted);
+            highscores.Sort();
+        }
+    }
+
+    void ResetHighscores()
+    {
+        highscores.Clear();
+    }
+}
