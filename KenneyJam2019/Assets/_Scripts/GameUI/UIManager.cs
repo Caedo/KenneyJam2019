9ddc18b3eab8a20368f3ddbe658578d9ee195093
AzuxDario <pawel_mibor95@tlen.pl>
2019-08-11 11:21:18 +0200

    Added highscores to main menu

--- KenneyJam2019/Assets/_Scripts/GameUI/UIManager.cs
+++ KenneyJam2019/Assets/_Scripts/GameUI/UIManager.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
-    public GameObject MainMenuPanel;
-    public GameObject GameSetupPanel;
-    public GameObject OptionsPanel;
-    public GameObject CreditsPanel;
-    public Text NumberOfNpcsValueLabel;
+
+    public RaceData raceData;
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject gameSetupPanel;
+    public GameObject highscoresPanel;
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+    [Header("Setup Panel")]
+    public Text numberOfNpcsValueLabel;
     public Slider npcSlider;
-    public InputField NameField;
-    public Dropdown ColorDropdown;
-    public Text NumberOfLapsValueLabel;
+    public InputField nameField;
+    public Dropdown colorDropdown;
+    public Text numberOfLapsValueLabel;
     public Slider lapsSlider;
     public AudioMixer mixer;
 
-    public RaceData raceData;
+[   Header("Highscores Panel")]
+    public Text highscoresEntriesLabel;   
 
     [Header("Transform targets")]
     public Camera mainCamera;
     public Transform mainTransform;
     public Transform startTransform;
+    public Transform highscoresTransform;
     public Transform creditsTransform;
     public Transform optionsTransform;
 
@@ -34,10 +43,11 @@ public class UIManager : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        MainMenuPanel.SetActive(true);
-        GameSetupPanel.SetActive(false);
-        OptionsPanel.SetActive(false);
-        CreditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        gameSetupPanel.SetActive(false);
+        highscoresPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
         Time.timeScale = 1;
 
         target = mainTransform;
@@ -52,22 +62,31 @@ public class UIManager : MonoBehaviour {
     // Main menu
 
     public void SetupGame() {
-        MainMenuPanel.SetActive(false);
-        GameSetupPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        gameSetupPanel.SetActive(true);
 
         target = startTransform;
     }
 
+    public void ShowHighscores() {
+        mainMenuPanel.SetActive(false);
+        highscoresPanel.SetActive(true);
+
+        SetHighscores();
+
+        target = highscoresTransform;
+    }
+
     public void ShowOptions() {
-        MainMenuPanel.SetActive(false);
-        OptionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
 
         target = optionsTransform;
     }
 
     public void ShowCredits() {
-        MainMenuPanel.SetActive(false);
-        CreditsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
 
         target = creditsTransform;
     }
@@ -83,22 +102,19 @@ public class UIManager : MonoBehaviour {
     // Setup Game
 
     public void SetNumberOfNpcs(float number) {
-        NumberOfNpcsValueLabel.text = ((int) number).ToString();
+        numberOfNpcsValueLabel.text = ((int) number).ToString();
     }
 
     public void SetNumberOfLaps(float number) {
-        NumberOfLapsValueLabel.text = ((int) number).ToString();
+        numberOfLapsValueLabel.text = ((int) number).ToString();
     }
 
     public void StartGame() {
-        // int numberOfNpcs = int.Parse(NumberOfNpcsValueLabel.text);
-        // string playerName = NameField.text;
-        string color = ColorDropdown.options[ColorDropdown.value].text;
+        string color = colorDropdown.options[colorDropdown.value].text;
         Color playerColor = ColorDictionary[color];
-        // int numberOfLaps = int.Parse(NumberOfLapsValueLabel.text);
 
         PlayerData playerData = new PlayerData() {
-            name = NameField.text,
+            name = nameField.text,
             steerByAI = false,
             color = playerColor,
         };
@@ -107,12 +123,35 @@ public class UIManager : MonoBehaviour {
         raceData.CreateData((int) npcSlider.value, playerData);
 
         SceneManager.LoadScene("RaceTest");
-        //Application.LoadLevel(Application.loadedLevel);
     }
 
     public void BackFromGameSetup() {
-        MainMenuPanel.SetActive(true);
-        GameSetupPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        gameSetupPanel.SetActive(false);
+
+        target = mainTransform;
+    }
+
+    // Highscores
+
+    public void SetHighscores() {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < 5; i++)
+        {
+            if(PlayerPrefs.HasKey("Highscore_time_" + i.ToString()))
+            {
+                float time = PlayerPrefs.GetFloat("Highscore_time_" + i.ToString());
+                string name = PlayerPrefs.GetString("Highscore_name_" + i.ToString());
+                builder.AppendFormat("{0}. {1} - {2:00.00} s\n", i + 1, name, time);
+            }
+        }
+
+        highscoresEntriesLabel.text = builder.ToString();
+    }
+
+    public void BackFromHighscores() {
+        mainMenuPanel.SetActive(true);
+        highscoresPanel.SetActive(false);
 
         target = mainTransform;
     }
@@ -124,8 +163,8 @@ public class UIManager : MonoBehaviour {
     }
 
     public void BackFromOptions() {
-        MainMenuPanel.SetActive(true);
-        OptionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
 
         target = mainTransform;
     }
@@ -133,8 +172,8 @@ public class UIManager : MonoBehaviour {
     // Credits
 
     public void BackFromCredits() {
-        MainMenuPanel.SetActive(true);
-        CreditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
 
         target = mainTransform;
     }
